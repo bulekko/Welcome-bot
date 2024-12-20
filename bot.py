30f42f60ac798408d272cec768682eef9bc0e208
bulekko <raubobruno@gmail.com>
2024-12-20 22:09:19 +0100

    bot.py

---
+++ bot.py
@@ -0,0 +1,43 @@
+#importy
+import discord
+from discord.ext import commands
+from discord import app_commands
+from discord.app_commands import Choice
+from tokeen import token
+import random
+
+#zmienne
+list = ['your phrases', 'your phrases']
+channel-id = #Welcome channel id in here
+
+#potrzebne
+intents = discord.Intents.all()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+bot.guilds
+
+#Wiadmomość uruchomienia
+@bot.event
+async def on_ready():
+    print("online")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Zynschronizowano {len(synced)}")
+    except Exception as e:
+        print(e)
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(channel-id)
+    
+    random_slowo = random.choice(list)
+    embed = discord.Embed(
+    title=f"Witaj {member.name}", 
+    description=random_world, 
+    color=discord.Color.green()
+    )
+
+    if channel:
+        await channel.send(embed=embed)
+
+bot.run(token)
